e807fa0ca53ca01593b334969e4621ef98b1fc2e
amiralqodri06 <43472160+amiralqodri06@users.noreply.github.com>
2019-01-25 11:14:26 +0700

    Update js-multi.js

--- js-multi.js
+++ js-multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2019/01/post-1.html"}
-if(x=="2"){xxx="/2019/01/post-2.html"}
-if(x=="3"){xxx="/2019/01/post-3.html"}
-if(x=="4"){xxx="/2019/01/post-4.html"}
-if(x=="5"){xxx="/2019/01/post-5.html"}a_to_ve[i].href="https://safelinkamir-12.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2019/01/here-are-10-types-of-medicinal-plants.html"}
+if(x=="2"){xxx="/2019/01/7-this-natural-way-is-effective-in.html"}
+if(x=="3"){xxx="/2019/01/here-are-10-ways-to-increase-iq.html"}
+if(x=="4"){xxx="/2019/01/7-these-ways-are-proven-powerful-to-get.html"}
+if(x=="5"){xxx="/2019/01/6-fruits-that-have-more-benefits-for.html"}a_to_ve[i].href="https://safelinkamir-12.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
