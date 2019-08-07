6ebed2bb26afd7d83a1d57c1c17bfb2b3d9f5751
amiralqodri06 <43472160+amiralqodri06@users.noreply.github.com>
2019-08-08 00:04:52 +0700

    Update js-info.js

--- js-info.js
+++ js-info.js
@@ -19,11 +19,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2019/07/info-one.html"}
 if(x=="2"){xxx="/2019/07/info-two.html"}
-if(x=="3"){xxx="/2019/07/info-three.html"}a_to_ve[i].href="https://doi-info.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+ if(x=="3"){xxx="/2019/07/info-three.html"}
+if(x=="4"){xxx="/2019/08/info-four.html"}a_to_ve[i].href="https://doi-info.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
